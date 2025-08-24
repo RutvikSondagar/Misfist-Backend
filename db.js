dc8b65ebb974084a185d3236321eab3f5d6e4eb3
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2025-08-24 18:52:02 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,20 @@
+// db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://naineshpumbhadiya:CYIMkRSgh5y8Ae5V@cluster0.wiok4ny.mongodb.net/Misfits?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
